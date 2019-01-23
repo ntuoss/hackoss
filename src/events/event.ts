e2e36675b7ece5b03c300c0899a249e26d6e2950
Clarence Castillo <clarencecastillo@outlook.com>
2019-01-23 23:38:13 +0800

    Change indentation from 2 lines to 4

--- src/events/event.ts
+++ src/events/event.ts
@@ -4,52 +4,52 @@ import { Organisation } from '../organisations/organisation';
 import { Artwork } from '../artworks/artwork';
 
 export class Event {
-  id: string;
-  tgif: number;
-  title: string;
-  speakers: Speaker[];
-  tagline: string;
-  banner: Artwork;
-  description: string;
-  prerequisites: Prerequisite[];
-  dependencies: Dependency[];
-  promotion: string;
-  venue: Location;
-  startTime: Date;
-  endTime: Date;
-  githubUrl: string;
-  status: EventStatus;
-  public: boolean;
-  external: boolean;
-  hasFood: boolean;
-  hasDrinks: boolean;
-  remarks: string;
-  eventbrite: Publication;
-  facebook: Publication;
+    id: string;
+    tgif: number;
+    title: string;
+    speakers: Speaker[];
+    tagline: string;
+    banner: Artwork;
+    description: string;
+    prerequisites: Prerequisite[];
+    dependencies: Dependency[];
+    promotion: string;
+    venue: Location;
+    startTime: Date;
+    endTime: Date;
+    githubUrl: string;
+    status: EventStatus;
+    public: boolean;
+    external: boolean;
+    hasFood: boolean;
+    hasDrinks: boolean;
+    remarks: string;
+    eventbrite: Publication;
+    facebook: Publication;
 }
 
 export class Speaker {
-  person: Person;
-  organisation: Organisation;
-  position: string;
+    person: Person;
+    organisation: Organisation;
+    position: string;
 }
 
 export class Dependency {
-  label: string;
-  specification: string;
-  referenceUrl: string;
+    label: string;
+    specification: string;
+    referenceUrl: string;
 }
 
 export class Prerequisite {
-  label: string;
-  proficiency: Proficiency;
-  referenceUrl: string;
+    label: string;
+    proficiency: Proficiency;
+    referenceUrl: string;
 }
 
 export class Publication {
-  id: string;
-  status: EventStatus;
-  url: string;
+    id: string;
+    status: EventStatus;
+    url: string;
 }
 
 export type Proficiency = 'basic' | 'intermediate' | 'advanced';
